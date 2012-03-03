4f1b436b20d9e86aad6b4afebcb5eb88a67b3aea
Kenan Yildirim <kenanyildirim@lavabit.com>
2012-03-03 11:24:40 -0500

    Remove all files not used in this fork

--- src/main/java/org/dynmap/bukkit/permissions/PermissionProvider.java
+++
@@ -1,7 +0,0 @@
-package org.dynmap.bukkit.permissions;
-
-import org.bukkit.command.CommandSender;
-
-public interface PermissionProvider {
-    boolean has(CommandSender sender, String permission);
-}
